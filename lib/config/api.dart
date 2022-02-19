b4ff12c012776cc83963572c1789f3c6411f61d3
Mohamed Alwhaidi <mo5-98@hotmail.com>
2022-02-19 22:33:33 +0200

    config example

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,5 +1,4 @@
-import 'package:fixit/core/env_helpers.dart';
-
 class ApiConfig {
-  static var baseUrl = env('API_BASE_URL');
+  // static var baseUrl = env('API_BASE_URL')??'http://10.0.2.2:8000/api/';
+  static var baseUrl = 'http://10.0.2.2:8000/api/';
 }
