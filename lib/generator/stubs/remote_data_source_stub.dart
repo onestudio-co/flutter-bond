494d0f77f8baf547ec23e6a231f7583d045cdc6f
Mohamed Alwhaidi <mo5-98@hotmail.com>
2022-03-08 22:48:22 +0200

    extends from DataSource to reach all typa of map response

--- lib/generator/stubs/remote_data_source_stub.dart
+++ lib/generator/stubs/remote_data_source_stub.dart
@@ -1,7 +1,9 @@
 String remoteDataSourceStub({required String name}) => '''
+import 'package:fixit/core/data_soruce.dart';
 import 'package:fixit/core/api_client.dart';
 
-class ${name}RemoteDataSource {
+
+class ${name}RemoteDataSource extends DataSource{
   final ApiClient _client;
 
   ${name}RemoteDataSource(this._client);
