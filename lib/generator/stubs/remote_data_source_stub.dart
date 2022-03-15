91437fd03c86889a094a3551515644ecd059e365
Mohamed Alwhaidi <mo5-98@hotmail.com>
2022-03-15 03:54:32 +0200

    added space after between DataSource and { symbo

--- lib/generator/stubs/remote_data_source_stub.dart
+++ lib/generator/stubs/remote_data_source_stub.dart
@@ -2,7 +2,7 @@ String remoteDataSourceStub({required String name}) => '''
 import 'package:fixit/core/data_soruce.dart';
 import 'package:fixit/core/api_client.dart';
 
-class ${name}RemoteDataSource extends DataSource{
+class ${name}RemoteDataSource extends DataSource {
   final ApiClient _client;
 
   ${name}RemoteDataSource(this._client);
