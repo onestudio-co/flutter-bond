5ccf9a8f75c2947fae13deccc27dc82fc7e9b93d
Mohamed AlWhaidi <mo5-98@hotmail.com>
2022-08-06 23:03:48 +0300

    add dark and light theme

---
+++ lib/core/theme/bond_dark_theme_data.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../resources/bond_colors.dart';
+import '../resources/bond_fonts.dart';
+import '../resources/bond_styles.dart';
+import '../resources/bond_text_theme.dart';
+
+ThemeData bondDarkThemeData() {
+  return ThemeData(
+    /// main color of the app
+    primaryColor: BondColors.green,
+    primaryColorLight: BondColors.cetaceanBlue,
+    primaryColorDark: BondColors.greyDark,
+    disabledColor: BondColors.persianGreen, // this for disabled button color
+    splashColor: BondColors.greyLight,
+    backgroundColor: BondColors.background,
+    scaffoldBackgroundColor: BondColors.green,
+
+    /// AppBar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: BondColors.greyLight,
+      elevation: 0,
+      titleTextStyle:
+          getBoldStyle(color: BondColors.green, fontSize: BondFontSize.s14),
+    ),
+
+    /// Button theme
+    buttonTheme: const ButtonThemeData(
+      shape: RoundedRectangleBorder(),
+      disabledColor: BondColors.greyLight,
+      buttonColor: BondColors.greenLight,
+    ),
+
+    /// Text Theme
+    textTheme: BondTextTheme(),
+  );
+}
