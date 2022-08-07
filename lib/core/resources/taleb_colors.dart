3017703b291bc8bf9446f39d73d23ad276dbb282
Mohamed AlWhaidi <mo5-98@hotmail.com>
2022-08-07 23:21:03 +0300

    add taleb colors

--- lib/core/resources/taleb_colors.dart
+++ lib/core/resources/taleb_colors.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 class TalebColors {
   static const Color white = Colors.white;
-  static const Color persianGreen = Color(0xFF02B39B);
-  static const Color green = Color(0xFF0FB7A0);
-  static const Color greenLight = Color(0xFFA7E4DC);
-  static const Color greyLight = Color(0xFFE6E6E6);
-  static const Color greyDark = Color(0xFFB8C9CC);
-  static const Color cetaceanBlue = Color(0xFF000333);
-  static const Color background = Color(0xFFF2F7F7);
+  static const Color blueRegular = Color(0xFF3FC8E8);
+  static const Color blueDark = Color(0xFF37B0CC);
+  static const Color blueExtraDark = Color(0xFF2A8499);
+  static const Color yellowRegular = Color(0xFFFFBB00);
+  static const Color yellowDark = Color(0xFFCCA400);
+  static const Color yellowExtraDark = Color(0xFF997B00);
+  static const Color dimGrayRegular = Color(0xFF65617A);
+  static const Color dimGrayDark = Color(0xFFA9A2CC);
+  static const Color dimGrayExtraDark = Color(0xFF7F7A99);
+  static const Color brightGray = Color(0xFFE8F0F2);
+  static const Color greyRegular = Color(0xFFC8CBCC);
+  static const Color greyLight = Color(0xFFF2F1ED);
+  static const Color gainsBoro = Color(0xFFE8F0F2);
+  static const Color ghostWhite = Color(0xFFF8F7FB);
+  static const Color spanishGray = Color(0xFF999999);
 }
--- lib/core/resources/taleb_text_theme.dart
+++ lib/core/resources/taleb_text_theme.dart
@@ -7,21 +7,21 @@ import 'taleb_styles.dart';
 class TalebTextTheme extends TextTheme {
   @override
   TextStyle get displayMedium => getMediumStyle(
-      fontSize: TalebFontSize.s20, color: TalebColors.persianGreen);
+      fontSize: TalebFontSize.s20, color: TalebColors.brightGray);
 
   @override
   TextStyle get displaySmall => getBoldStyle(
-      fontSize: TalebFontSize.s16, color: TalebColors.persianGreen);
+      fontSize: TalebFontSize.s16, color: TalebColors.blueRegular);
 
   @override
   TextStyle get headlineMedium => getMediumStyle(
-      fontSize: TalebFontSize.s16, color: TalebColors.persianGreen);
+      fontSize: TalebFontSize.s16, color: TalebColors.blueRegular);
 
   @override
   TextStyle get headlineSmall => getTextStyle(
-      fontSize: TalebFontSize.s16, color: TalebColors.persianGreen);
+      fontSize: TalebFontSize.s16, color: TalebColors.brightGray);
 
   @override
   TextStyle get titleMedium => getMediumStyle(
-      fontSize: TalebFontSize.s14, color: TalebColors.persianGreen);
+      fontSize: TalebFontSize.s14, color: TalebColors.dimGrayRegular);
 }
