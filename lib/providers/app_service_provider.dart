c87660fec72c76f44a4548027b3db3c159c5c533
Mohamed AlWhaidi <mo5-98@hotmail.com>
2022-08-13 18:33:45 +0300

    add prettyDioLogger

--- lib/providers/app_service_provider.dart
+++ lib/providers/app_service_provider.dart
@@ -1,7 +1,6 @@
-import 'package:get_it/get_it.dart';
 import 'package:one_studio_core/core.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:taleb/config/api.dart';
 
 import '../routes/app_router.dart';
 import '../routes/guards/auth_guard.dart';
@@ -9,7 +8,27 @@ import '../routes/guards/auth_guard.dart';
 class AppServiceProvider extends ServiceProvider {
   @override
   Future<void> register(GetIt it) async {
-    final sharedPreferences = await SharedPreferences.getInstance();
+    it.registerSingleton(
+      () => Dio(
+        BaseOptions(
+          connectTimeout: 100 * 1000,
+          sendTimeout: 1000 * 1000,
+          receiveTimeout: 1000 * 1000,
+          receiveDataWhenStatusError: true,
+          baseUrl: ApiConfig.baseUrl,
+        ),
+      )..interceptors.add(
+          PrettyDioLogger(
+            requestHeader: true,
+            requestBody: true,
+            responseBody: true,
+            responseHeader: false,
+            compact: false,
+          ),
+        ),
+    );
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
     it.registerLazySingleton(() => sharedPreferences);
 
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -6,3 +6,6 @@ export 'src/injection.dart';
 export 'src/network.dart';
 export 'src/utils.dart';
 export 'package:get_it/get_it.dart';
+export 'package:package_info_plus/package_info_plus.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:dio/dio.dart';
