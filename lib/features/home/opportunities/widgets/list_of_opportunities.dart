f820935e669ed6e004d6cd3d350e8adbad796796
MohamedAlwhaidi <mo5-98@hotmail.com>
2022-08-15 13:08:27 +0300

    add opp details

--- lib/features/home/opportunities/widgets/list_of_opportunities.dart
+++ lib/features/home/opportunities/widgets/list_of_opportunities.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taleb/core/resources/import_resources.dart';
-import 'package:taleb/core/widget/taleb_container.dart';
-import 'package:taleb/features/home/news/ui/widgets/news_image.dart';
-import 'package:taleb/main.dart';
 
-import 'container_tag.dart';
+import '../ui/all_opportunities/widgets/opportunity_card.dart';
 
 class ListOfOpportunities extends StatelessWidget {
   const ListOfOpportunities({
@@ -18,57 +15,8 @@ class ListOfOpportunities extends StatelessWidget {
       child: ListView.builder(
         scrollDirection: Axis.vertical,
         itemCount: 10,
-        itemBuilder: (BuildContext context, int index) => TalebContainer(
-          padding: EdgeInsets.all(TalebPadding.p12),
-          margin: EdgeInsets.only(bottom: TalebPadding.p8),
-          child: Row(
-            children: [
-              Hero(
-                tag: index,
-                child: TalebImageWidget(
-                  image: url,
-                  width: TalebSizes.w145,
-                  height: TalebSizes.h108,
-                ),
-              ),
-              HorizontalSpace(TalebSizes.w12),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        ContainerTagWidget(
-                          title: 'فرص أكاديمية',
-                        ),
-                        ContainerTagWidget(
-                          title: 'الطفيلة',
-                        ),
-                      ],
-                    ),
-                    VerticalSpace(TalebSizes.h8),
-                    Text(
-                      'لقاء ارشادي و توجيهي لطلبة توجيهي 2005',
-                      style:
-                          Theme.of(context).textTheme.labelSmall?.chathamsBlue,
-                    ),
-                    VerticalSpace(TalebSizes.h4),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        '2 أغسطس 2022| 12:34 مساءً',
-                        style:
-                            Theme.of(context).textTheme.displaySmall?.coolGrey,
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+        itemBuilder: (BuildContext context, int index) =>
+            OpportunityCard(index: index),
       ),
     );
   }
