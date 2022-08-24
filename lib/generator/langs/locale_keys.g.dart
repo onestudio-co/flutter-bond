ce3dd9111487efd6ce8549b95c3eab8d9ca1ca8e
Mohamed AlWhaidi <mo5-98@hotmail.com>
2022-08-24 23:08:03 +0300

    add localization files

---
+++ lib/generator/langs/locale_keys.g.dart
@@ -0,0 +1,9 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const noInternet = 'noInternet';
+  static const reconnect = 'reconnect';
+  static const homeTitle = 'homeTitle';
+  static const categoryTitle = 'categoryTitle';
+
+}
