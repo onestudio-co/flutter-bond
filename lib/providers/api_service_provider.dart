e0cd98b71deae94242b76f84a85b25025eddc7d8
Mohamed AlWhaidi <mo5-98@hotmail.com>
2022-08-27 09:29:34 +0300

    edit dio logger

--- lib/providers/api_service_provider.dart
+++ lib/providers/api_service_provider.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:one_studio_core/core.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../config/api.dart';
 
@@ -14,8 +16,19 @@ class ApiServiceProvider extends ServiceProvider {
       receiveDataWhenStatusError: true,
       baseUrl: ApiConfig.baseUrl,
     );
-
     it.registerLazySingleton(() => Dio(baseOptions));
+
+    if (!kReleaseMode) {
+      it<Dio>().interceptors.add(
+            PrettyDioLogger(
+              requestHeader: true,
+              requestBody: true,
+              responseBody: true,
+              responseHeader: false,
+              compact: false,
+            ),
+          );
+    }
     it.registerLazySingleton(() => ApiClient(it()));
   }
 }
