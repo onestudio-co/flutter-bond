8fcf3a3fa7bba5714bbfc170415235b97d77f043
Salah Nahed <salahamassi@users.noreply.github.com>
2022-09-26 03:22:18 +0400

    update FirebaseAnalyticsProvider

--- lib/core/app_analytics_providers/firebase_analytics_provider.dart
+++ lib/core/app_analytics_providers/firebase_analytics_provider.dart
@@ -3,79 +3,99 @@ import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 import 'package:one_studio_core/core.dart';
 
-class FirebaseAnalyticsProvider implements AnalyticsProvider {
+class FirebaseAnalyticsProvider extends AnalyticsProvider {
   FirebaseAnalyticsProvider(this._firebaseAnalytics);
 
   final FirebaseAnalytics _firebaseAnalytics;
 
   @override
-  void log(AnalyticsEvent event) {
+  void logEvent(AnalyticsEvent event) {
     final String eventKey = event.key.toLowerCase().replaceAll(' ', '_');
-    if (event is UserLoggedIn) {
-      final UserLoggedIn userLoggedIn = event as UserLoggedIn;
-      _firebaseAnalytics.setUserId(id: userLoggedIn.id.toString());
-      _firebaseAnalytics.logLogin(loginMethod: userLoggedIn.loginMethod);
-    } else if (event is UserSignedUp) {
-      final UserSignedUp userSignedUp = event as UserSignedUp;
-      _firebaseAnalytics.setUserId(id: userSignedUp.id.toString());
-      _firebaseAnalytics.logSignUp(signUpMethod: userSignedUp.signupMethod);
-    } else if (event is UserUpdateProfile) {
-      final UserUpdateProfile user = event as UserUpdateProfile;
-      _firebaseAnalytics.setUserProperty(name: 'name', value: user.name);
-      _firebaseAnalytics.setUserProperty(name: 'mobile', value: user.mobile);
-      _firebaseAnalytics.setUserProperty(name: 'email', value: user.email);
-      _firebaseAnalytics.setUserProperty(name: 'dob', value: user.dob);
-      _firebaseAnalytics.setUserProperty(name: 'gender', value: user.gender);
-      _firebaseAnalytics.setUserProperty(name: 'country', value: user.country);
-      _firebaseAnalytics.setUserProperty(name: 'city', value: user.city);
-      _firebaseAnalytics.setUserProperty(
-          name: 'age', value: user.age.toString());
-      user.customAttributes.forEach(_sendCustomAttributes);
-      _firebaseAnalytics.logEvent(name: eventKey, parameters: event.params);
-    } else if (event is UserViewedItemList) {
-      _firebaseAnalytics.logViewItemList();
-    } else if (event is UserAddedToCart) {
-      final UserAddedToCart userAddedToCart = event as UserAddedToCart;
-      _firebaseAnalytics.logAddToCart(
-          items: userAddedToCart.items
-              .map((CartItem item) => item.analyticsEventItem)
-              .toList(),
-          value: userAddedToCart.value,
-          currency: userAddedToCart.currency);
-    } else if (event is UserBeginCheckout) {
-      final UserBeginCheckout userBeginCheckout = event as UserBeginCheckout;
-      _firebaseAnalytics.logBeginCheckout(
-        value: userBeginCheckout.value,
-        currency: userBeginCheckout.currency,
-        items: userBeginCheckout.items
+    _firebaseAnalytics.logEvent(
+      name: eventKey,
+      parameters: event.params,
+    );
+  }
+
+  @override
+  void logBeginTutorial() {
+    _firebaseAnalytics.logTutorialBegin();
+  }
+
+  @override
+  void logCompleteTutorial() {
+    _firebaseAnalytics.logTutorialComplete();
+  }
+
+  @override
+  void logSignedIn(UserLoggedIn event) {
+    _firebaseAnalytics.setUserId(id: event.id.toString());
+    _firebaseAnalytics.logLogin(loginMethod: event.loginMethod);
+  }
+
+  @override
+  void logSignedUp(UserSignedUp event) {
+    _firebaseAnalytics.setUserId(id: event.id.toString());
+    _firebaseAnalytics.logSignUp(signUpMethod: event.signupMethod);
+  }
+
+  @override
+  void updateProfile(UserUpdateProfile event) {
+    _firebaseAnalytics.setUserProperty(name: 'name', value: event.name);
+    _firebaseAnalytics.setUserProperty(name: 'mobile', value: event.mobile);
+    _firebaseAnalytics.setUserProperty(name: 'email', value: event.email);
+    _firebaseAnalytics.setUserProperty(name: 'dob', value: event.dob);
+    _firebaseAnalytics.setUserProperty(name: 'gender', value: event.gender);
+    _firebaseAnalytics.setUserProperty(name: 'country', value: event.country);
+    _firebaseAnalytics.setUserProperty(name: 'city', value: event.city);
+    _firebaseAnalytics.setUserProperty(
+        name: 'age', value: event.age.toString());
+    event.customAttributes.forEach(_sendCustomAttributes);
+  }
+
+  @override
+  void logViewItemList() {
+    _firebaseAnalytics.logViewItemList();
+  }
+
+  @override
+  void logAddToCart(UserAddedToCart event) {
+    _firebaseAnalytics.logAddToCart(
+        items: event.items
             .map((CartItem item) => item.analyticsEventItem)
             .toList(),
-      );
-    } else if (event is UserMadePurchase) {
-      final UserMadePurchase userMadePurchase = event as UserMadePurchase;
-      _firebaseAnalytics.logPurchase(
-        transactionId: userMadePurchase.transactionId,
-        value: userMadePurchase.value,
-        tax: userMadePurchase.tax,
-        currency: userMadePurchase.currency,
-        coupon: userMadePurchase.coupon,
-        items: userMadePurchase.items
-            .map((CartItem item) => item.analyticsEventItem)
-            .toList(),
-      );
-    } else if (event is UserBeginTutorial) {
-      _firebaseAnalytics.logTutorialBegin();
-    } else if (event is UserCompleteTutorial) {
-      _firebaseAnalytics.logTutorialComplete();
-    } else {
-      _firebaseAnalytics.logEvent(name: eventKey, parameters: event.params);
-    }
+        value: event.value,
+        currency: event.currency);
+  }
+
+  @override
+  void logBeginCheckout(UserBeginCheckout event) {
+    _firebaseAnalytics.logBeginCheckout(
+      value: event.value,
+      currency: event.currency,
+      items:
+          event.items.map((CartItem item) => item.analyticsEventItem).toList(),
+    );
+  }
+
+  @override
+  void logMadePurchase(UserMadePurchase event) {
+    _firebaseAnalytics.logPurchase(
+      transactionId: event.transactionId,
+      value: event.value,
+      tax: event.tax,
+      currency: event.currency,
+      coupon: event.coupon,
+      items:
+          event.items.map((CartItem item) => item.analyticsEventItem).toList(),
+    );
   }
 
   void _sendCustomAttributes(String key, dynamic value) {
     var userPropertyKey = key.toLowerCase().replaceAll(' ', '_');
-    debugPrint(
-        'FirebaseAnalytics send custom user attribute with key: $userPropertyKey, value: $value and value type: ${value.runtimeType}');
+    debugPrint('FirebaseAnalytics send custom user attribute with key: '
+        '$userPropertyKey, '
+        'value: $value and value type: ${value.runtimeType}');
     if (value is DateTime) {
       final DateFormat formatter =
           DateFormat("'~t'yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
