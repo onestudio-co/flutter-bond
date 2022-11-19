af8dcc3fc6c2b3066c2fd4670c1a10b748c8cc03
Salah Nahed <salahamassi@users.noreply.github.com>
2022-11-19 22:09:10 +0200

    remove unused files

--- lib/app/injection_container.dart
+++ lib/app/injection_container.dart
@@ -1,9 +1,6 @@
 import 'package:one_studio_core/core.dart' as core;
 
 import '../config/app.dart';
-import '../features/app/bloc/app_bloc.dart';
-
-App appBloc = App();
 
 Future<void> init() async {
   for (var provider in providers) {
