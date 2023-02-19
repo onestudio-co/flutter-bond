dba82066c35bb9ce21113894f06a4b82dd18de22
MahmoudHafezDarwish <mahm.dar99@gmail.com>
2023-02-19 10:20:06 +0200

    :construction: Use riverpod in collections page

--- lib/features/post/post_service_provider.dart
+++ lib/features/post/post_service_provider.dart
@@ -1,3 +1,4 @@
+import 'package:bond/features/post/data/datasource/post_client.dart';
 import 'package:bond/features/post/presentation/cubit/post_cubit.dart';
 import 'package:get_it/get_it.dart';
 import 'package:one_studio_core/core.dart';
@@ -8,6 +9,7 @@ class PostServiceProvider extends ServiceProvider {
   @override
   Future<void> register(GetIt it) async {
     it.registerFactory(() => PostCubit());
+    it.registerFactory(() => PostClient(it()));
   }
 
   @override
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,5 +1,6 @@
 import 'package:bond/routes/app_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:one_studio_core/core.dart';
 
 import 'app/app_run_tasks.dart';
@@ -9,8 +10,10 @@ import 'features/app/app_bloc.dart';
 void main() => run(
       () => BlocProvider<AppBloc>(
         create: (context) => sl<AppBloc>(),
-        child: BondApp(
-          appRouter: sl<AppRouter>(),
+        child: ProviderScope(
+          child: BondApp(
+            appRouter: sl<AppRouter>(),
+          ),
         ),
       ),
       RunAppTasks(),
