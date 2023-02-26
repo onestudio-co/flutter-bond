62136774dfb9f76518c9454fb46dc8830e650c19
MahmoudHafezDarwish <mahm.dar99@gmail.com>
2023-02-26 13:34:26 +0200

    :fire: remove repository and usage of it

--- lib/features/auth/auth_service_provider.dart
+++ lib/features/auth/auth_service_provider.dart
@@ -2,18 +2,14 @@ import 'package:bond/features/auth/data/datasource/auth_http_client.dart';
 import 'package:get_it/get_it.dart';
 import 'package:one_studio_core/core.dart';
 
-import 'data/datasource/auth_remote_data_source.dart';
 import 'data/models/user.dart';
 import 'data/models/user_meta.dart';
-import 'data/repositories/auth_repository.dart';
-import 'presentation/login/login_bloc.dart';
 import 'presentation/logout/logout_cubit.dart';
 import 'presentation/register/register_bloc.dart';
 
 class AuthServiceProvider extends ServiceProvider {
   @override
   Future<void> register(GetIt it) async {
-    it.registerFactory(() => AuthRemoteDataSource(it()));
     it.registerLazySingleton(
       () => AuthStore<User>(
         it(),
@@ -22,10 +18,6 @@ class AuthServiceProvider extends ServiceProvider {
       ),
     );
     it.registerLazySingleton(() => AuthHttpClient(it()));
-
-    it.registerLazySingleton(() => AuthRepository(it(), it()));
-
-    it.registerFactory(() => LoginBloc(it()));
 
     it.registerFactory(() => RegisterBloc(it()));
 
--- lib/features/auth/presentation/register/register_bloc.dart
+++ lib/features/auth/presentation/register/register_bloc.dart
@@ -1,11 +1,10 @@
+import 'package:bond/features/auth/data/datasource/auth_http_client.dart';
 import 'package:bond/features/auth/data/dto/user_dto.dart';
-import 'package:bond/features/auth/data/repositories/auth_repository.dart';
 import 'package:bond/features/auth/data/validators/field_validators.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
-import 'package:one_studio_core/core.dart';
 
 class RegisterBloc extends FormBloc<String, String> {
-  final AuthRepository _authRepository;
+  final AuthHttpClient _authRepository;
 
   RegisterBloc(this._authRepository) {
     addFieldBlocs(
@@ -48,33 +47,36 @@ class RegisterBloc extends FormBloc<String, String> {
   @override
   void onSubmitting() async {
     emitLoading();
-    final response = await _authRepository.register(
+    await _authRepository.register(
       UserDto(
         name: nameTextField.value,
         email: emailTextField.value,
         password: passwordTextField.value,
         passwordConfirmation: confirmPassword.value,
-      ),
+      ).body,
     );
-    response.fold(
-      (failure) => _handleFailure(failure),
-      (response) => {emitLoaded(), emitSuccess(), emitSubmissionCancelled()},
-    );
+    emitLoaded();
+    emitSuccess();
+    emitSubmissionCancelled();
+    // response.fold(
+    //   (failure) => _handleFailure(failure),
+    //   (response) => {emitLoaded(), emitSuccess(), emitSubmissionCancelled()},
+    // );
   }
 
-  void _handleFailure(Failure failure) {
-    if (failure is ServerFailure && failure.code == 422) {
-      failure.errors.forEach((key, value) {
-        _mapOfTextFieldBloc[key].addFieldError(value.join(', '));
-      });
-    }
-    emitFailure(failureResponse: failure.toMessage());
-  }
+  // void _handleFailure(Failure failure) {
+  //   if (failure is ServerFailure && failure.code == 422) {
+  //     failure.errors.forEach((key, value) {
+  //       _mapOfTextFieldBloc[key].addFieldError(value.join(', '));
+  //     });
+  //   }
+  //   emitFailure(failureResponse: failure.toMessage());
+  // }
 
   bool get loading => state is FormBlocLoading;
 
-  Map<String, dynamic> get _mapOfTextFieldBloc => {
-        'email': emailTextField,
-        'password': passwordTextField,
-      };
+  // Map<String, dynamic> get _mapOfTextFieldBloc => {
+  //       'email': emailTextField,
+  //       'password': passwordTextField,
+  //     };
 }
