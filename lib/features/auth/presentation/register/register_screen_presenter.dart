9b220dbfce06d5930e375214d725b2b887b5a064
Salah Nahed <salahamassi@users.noreply.github.com>
2023-02-28 11:45:50 +0200

    🎨 improve code
    Co-Authored-By: Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>

--- lib/features/auth/presentation/register/register_screen_presenter.dart
+++ lib/features/auth/presentation/register/register_screen_presenter.dart
@@ -1,18 +1,36 @@
 import 'package:bond/core/utils/validator.dart';
+import 'package:bond/features/auth/data/dto/user_dto.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'register_request_provider.dart';
+
+// 3- TODO: how to test this logic
 final registerScreenPresenter =
     ChangeNotifierProvider.autoDispose<RegisterScreenPresenter>(
   (ref) => RegisterScreenPresenter(),
 );
 
+enum RegisterScreenPresenterState { initial, loading, success, error }
+
 class RegisterScreenPresenter extends ChangeNotifier {
+  final nameController = useTextEditingController();
+  final emailController = useTextEditingController();
+  final passwordController = useTextEditingController();
+  final passwordConfirmationController = useTextEditingController();
+  final _state = useState(RegisterScreenPresenterState.initial);
+
+  RegisterScreenPresenterState get state => _state.value;
+
+  bool get loading => state == RegisterScreenPresenterState.loading;
+
   bool _isNameValid = true;
   bool _isEmailValid = true;
   bool _isPasswordValid = true;
   bool _isPasswordConfirmationValid = true;
 
+  // 2- TODO: read errors message directly from app localizations
   String? getNameErrorText(String errorMessage) =>
       _isNameValid ? null : errorMessage;
 
@@ -25,20 +43,36 @@ class RegisterScreenPresenter extends ChangeNotifier {
   String? getPasswordConfirmationErrorText(String errorMessage) =>
       _isPasswordConfirmationValid ? null : errorMessage;
 
-  bool checkEmailAndPassword({
-    required String name,
-    required String email,
-    required String password,
-    required String confirmPassword,
-  }) {
-    _isNameValid = Validator.isFullNameValid(name);
-    _isEmailValid = Validator.isEmailValid(email);
-    _isPasswordValid = Validator.isPasswordValid(password);
-    _isPasswordConfirmationValid = password == confirmPassword;
-    notifyListeners();
-    return _isNameValid &&
+  void register(WidgetRef ref) {
+    _isNameValid = Validator.isFullNameValid(nameController.text);
+    _isEmailValid = Validator.isEmailValid(emailController.text);
+    _isPasswordValid = Validator.isPasswordValid(passwordController.text);
+    _isPasswordConfirmationValid =
+        passwordController.text == passwordConfirmationController.text;
+    final isValid = _isNameValid &&
         _isEmailValid &&
         _isPasswordValid &&
         _isPasswordConfirmationValid;
+
+    if (isValid) {
+      final requestProvider = ref.watch(registerRequestProvider(userDto));
+
+      requestProvider.when(
+          data: (_) {
+            _state.value = RegisterScreenPresenterState.success;
+          },
+          error: (_, __) {
+            _state.value = RegisterScreenPresenterState.error;
+          },
+          loading: () => _state.value = RegisterScreenPresenterState.loading);
+    }
+    notifyListeners();
   }
+
+  UserDto get userDto => UserDto(
+        email: emailController.text,
+        password: passwordController.text,
+        name: nameController.text,
+        passwordConfirmation: passwordConfirmationController.text,
+      );
 }
