dacfc0d426a2d92811674818f2efd13d1fd97003
MahmoudHafezDarwish <mahm.dar99@gmail.com>
2023-03-01 13:32:02 +0200

    :bento: add share button

--- lib/core/resources/app_icons.dart
+++ lib/core/resources/app_icons.dart
@@ -1,4 +1,5 @@
 class AppIcons {
   static const String test = 'assets/icons/test.svg';
   static const String close = 'assets/icons/close.svg';
+  static const String share = 'assets/icons/share.svg';
 }
---
+++ lib/features/post_details/presentation/widgets/share_button.dart
@@ -0,0 +1,29 @@
+import 'package:bond/core/resources/app_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:one_studio_core/core.dart';
+
+class ShareButton extends StatelessWidget {
+  const ShareButton({
+    required this.onShare,
+    Key? key,
+  }) : super(key: key);
+
+  final VoidCallback onShare;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      splashRadius: 20,
+      onPressed: () => _onShare(context),
+      icon: SvgPicture.asset(
+        AppIcons.share,
+        color: context.colorScheme.primary,
+      ),
+    );
+  }
+
+  void _onShare(BuildContext context) {
+    onShare();
+  }
+}
