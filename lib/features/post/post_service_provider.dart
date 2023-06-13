958bac1c0701d29e314a8e8086a7d15dce3d56af
Salah Nahed <salahamassi@users.noreply.github.com>
2023-06-13 22:58:56 +0300

    remove retort package and use bond fire

--- lib/features/post/post_service_provider.dart
+++ lib/features/post/post_service_provider.dart
@@ -1,15 +1,15 @@
-import 'package:bond/features/post/data/datasource/post_client.dart';
 import 'package:bond/features/post/presentation/cubit/post_cubit.dart';
-import 'package:get_it/get_it.dart';
 import 'package:bond_core/core.dart';
+import 'package:get_it/get_it.dart';
 
+import 'data/api.dart';
 import 'data/models/post.dart';
 
 class PostServiceProvider extends ServiceProvider {
   @override
   Future<void> register(GetIt it) async {
-    it.registerFactory(() => PostCubit());
-    it.registerFactory(() => PostClient(it()));
+    it.registerFactory(() => PostsApi(it()));
+    it.registerFactory(() => PostCubit(it()));
   }
 
   @override
