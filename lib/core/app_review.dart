33a66f2a3dde014a0de8fe1e2934035e4f351309
Hala S.A. Abusalim <hala.s.salim@gmail.com>
2023-06-27 01:56:29 +0300

    check isAvailable value

--- lib/core/app_review.dart
+++ lib/core/app_review.dart
@@ -1,8 +1,14 @@
 import 'package:in_app_review/in_app_review.dart';
 
 class AppReviewHelper {
-  static void showAppReview() async {
-    await InAppReview.instance.isAvailable();
-    InAppReview.instance.requestReview();
+  static Future<bool> showAppReview() async {
+    bool isAvailable = await InAppReview.instance.isAvailable();
+
+    if (isAvailable) {
+      await InAppReview.instance.requestReview();
+      return true;
+    } else {
+      return false;
+    }
   }
 }
